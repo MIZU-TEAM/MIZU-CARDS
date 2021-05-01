ac6774b25cc03bef9bc685ed2aa6c7e7643a8db7
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2021-05-01 11:05:14 +0100

    Update galleryupdater.py

--- galleryupdater.py
+++ galleryupdater.py
@@ -15,9 +15,9 @@ for i in range(0, directory.__len__()):
 for i in range(0, images.__len__()):
     githubFileName = images[i].replace("+", "%2B")
     if i == 0 or i % 3 == 0:
-        mdFile = mdFile + f'\n<img src="https://github.com/MIZU-TEAM/MIZU-CARDS/blob/main/custom-cards/{githubFileName}" width="300">'
+        mdFile = mdFile + f'\n<img src="https://github.com/MIZU-TEAM/MIZU-CARDS/blob/main/cards/{githubFileName}" width="300">'
     else:
-        mdFile = mdFile + f'<img src="https://github.com/MIZU-TEAM/MIZU-CARDS/blob/main/custom-cards/{githubFileName}" width="300">'
+        mdFile = mdFile + f'<img src="https://github.com/MIZU-TEAM/MIZU-CARDS/blob/main/cards/{githubFileName}" width="300">'
 
 
 galleryFile.write(mdFile)
